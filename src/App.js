8240de3ba62edc5dbe96dc72b3df8f13a1c3162e
Elisa Edson <e.edson@protonmail.ch>
2023-07-24 22:53:24 +0100

    architecture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./modules/header.js";
+import Footer from "./modules/footer.js";
+import Navbar from "./modules/navbar.js"
+import Main from "./modules/main.js"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Navbar/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/modules/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Index from "./pages/Index.js"
+import AboutMe from './pages/AboutMe.js'
+import Intsum from './pages/Intsum'
+import Nations from './pages/Nations.js'
+
+export default function main() {
+  return (
+    <div>
+        <Index/>
+    </div>
+  )
+}
