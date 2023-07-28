de0f1c3772d5ec7aee26e7269ff931ecfe8bb5e2
Elisa Edson <e.edson@protonmail.ch>
2023-07-28 15:19:26 +0100

    retrieve nation data from backend

--- src/modules/header.js
+++ src/modules/header.js
@@ -3,10 +3,14 @@ import ButtonDay from "./images/button-day.png"
 import ButtonNight from "./images/button-night.png"
 
 export default function header() {
+  function darkMode(){
+
+  };
+
   return (
     <header>
         ☆☆Circus World☆☆
-        <button id="darkmodebutton" onclick="darkMode()"><img id="buttonimage" src="images/button-day.png"/></button>
+        <button id="darkmodebutton" onClick={darkMode()}><img id="buttonimage" src="images/button-day.png"/></button>
     </header>
   )
 }
--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React from 'react';
+//import {useState} from "react";
+import axios from "axios";
 
 export default function navbar() {
+  async function getAPI(){
+    const port =  8095;
+    //temporary dev address - change on live
+    const API = `http://localhost:${port}/`;
+    let result = await axios.get(API);
+    console.log(result.data[0]);
+  }
+  getAPI();
   return (
     <div>navbar</div>
   )
